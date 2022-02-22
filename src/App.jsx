e49389db45bdce9c08275a7b0a6af8e1beffebe8
KevinRaleie-dev <kkevraleie@gmail.com>
2022-02-22 10:33:17 +0200

    dynamically show current month & removed text

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,8 @@ const fetcher = async (url) => {
   return response.data;
 }
 
+const month = new Date().getMonth()
+
 function App() {
   const devURL = 'http://localhost:4000/bursaries/?search=';
   const prodURL = 'https://bursary-findr.herokuapp.com/bursaries/?search=';
@@ -92,11 +94,10 @@ function App() {
           <div className="grid place-items-center space-y-2 w-full">
             {/* <p className="text-sm text-gray-500 text-center">Search and find bursaries in South Africa by the month they'll be closing.</p> */}
             <p className="text-lg text-black font-semibold text-center">✨What's New✨</p>
-            <p className="text-sm text-center font-medium opacity-80">👉 New bursaries for the year 2022!</p>
-            <p className="text-sm text-center font-medium opacity-80">Click on January and February or search 🤫 Don't say we didn't tell you.</p>
+            <p className="text-sm text-center font-medium opacity-80">🔔 New bursaries for the year 2022!</p>        
             <input className="hover:shadow text-sm border px-3 w-80 h-10 sm:w-1/2 sm:h-12 rounded-full focus:outline-none" 
               type="search" 
-              placeholder="Try searching 'Bursaries closing in January 2022'..."
+              placeholder={`Try searching 'Bursaries closing in ${months[month].month} 2022'...`}
               onChange={(e) => setSearch(e.currentTarget.value)}
               />
           </div>
